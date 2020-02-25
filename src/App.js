155f4af5bbb858acf294d14ab3d60f87820e5905
Tristancht <tristan@hemka.com>
2020-02-25 14:26:37 +0100

    link

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import AppRouter from "./components/AppRouter";
 
+
 const App = () => {
   return <AppRouter />;
 };
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,16 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../css/menu.css";
 
 const Menu = () => {
 
   return (
     <nav className="menu">
-      <Link to="/">
-        <h3>Home</h3>
-      </Link>
-      <Link to="/Concept">
-        <h3>Concept</h3>
-      </Link>
+      <div className="imgMenu"></div>
+
+      <div className="menuLink">
+        <Link to="/">
+          <h3>Home</h3>
+        </Link>
+        <Link to="/Concept">
+          <h3>Concept</h3>
+        </Link>
+        <Link to="/Concept">
+          <h3>Nos aventures</h3>
+        </Link>
+        <Link to="/Concept">
+          <h3>Tarif/Reseau</h3>
+        </Link>
+        <Link to="/Concept">
+          <h3>Contact</h3>
+        </Link>
+        <Link to="/Panier">
+          <h3>Contact</h3>
+        </Link>
+      </div>
     </nav>
   );
 };
