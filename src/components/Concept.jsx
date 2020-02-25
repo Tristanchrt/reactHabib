a07a333a6d32ac0c89a9bddb0bccef92691f5da5
Habib <houideghabib@gmail.com>
2020-02-25 14:53:39 +0100

    conceptV2

--- src/components/Concept.jsx
+++ src/components/Concept.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import logo from './logo.png';
 const Concept = () => {
   return (
     <div>
@@ -7,7 +6,7 @@ const Concept = () => {
       <p>Le principe de base est simple: vous êtes enfermés dans une salle (en groupe de 2 à 7 joueurs) et vous avez une heure pour vous échapper. Afin d'y parvenir, vous devrez résoudre des énigmes.  Quoiqu'il se passe, dans ce jeu, la force physique n'a aucune importance. Il s'agit d'une activité calme et réfléchie qui fait appel aux capacités d'observation et de déduction ainsi qu'au bon sens.
 
 Que ce soit pour une soirée entre amis ou un événement d'entreprise, vous découvrirez une nouvelle façon de jouer et de vous évader, dans tous les sens du terme !</p>
-<img src="../Door.PNG"alt="Porte ouverte"></img>
+<img src="Escape/reacthabib/Door.PNG"alt="Porte ouverte"></img>
     </div>
   );
 };
