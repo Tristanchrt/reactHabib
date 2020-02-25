eb2d4497196687992693543b7afc8f4827331f68
Tristancht <tristan@hemka.com>
2020-02-25 14:51:36 +0100

    menu v1

--- src/components/Concept.jsx
+++ src/components/Concept.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import logoSablier from "../image/s.png";
 
 const Concept = () => {
   return (
     <div>
       <h1>Concept</h1>
+      <img className="imgMenuSablier" src={logoSablier} alt="Logo-sablier" />
     </div>
   );
 };
